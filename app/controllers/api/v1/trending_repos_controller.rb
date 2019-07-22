2692dc1cd4924397eb22a007c4ed4796d588e19f
sromelus <rshardlypicard89@gmail.com>
2019-07-21 22:46:42 -0400

    display repo writen languages

--- app/controllers/api/v1/trending_repos_controller.rb
+++ app/controllers/api/v1/trending_repos_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::TrendingReposController < ApplicationController
 
   def index
+    # Trending_repo.destroy_all
+    # Rails.application.load_seed
     render json: Trending_repo.all
   end
 
--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repos: []
+    }
+  }
+
+  componentDidMount(){
+   fetch("/api/v1/trending_repos")
+     .then(response => {
+       if (response.ok) {
+         return response;
+       } else {
+         let errorMessage = `${response.status}(${response.statusText})` ,
+         error = new Error(errorMessage);
+         throw(error);
+       }
+       })
+       .then(response => response.json())
+       .then(body => {
+         this.setState({
+           repos: body
+         })
+       })
+       .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
+  render(){
+    debugger
+    return(
+      <div>
+      {this.state.repos[0].name}
+      </div>
+    )
+  }
 }
 
-export default App
+export default App;
